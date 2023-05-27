da30d355331452b3957bc818e1f6b3adf6b1be50
lucky <singhlucky007007@gmail.com>
2023-05-28 01:44:08 +0530

    added favicon

--- src/app/statewebsites/page.jsx
+++ src/app/statewebsites/page.jsx
@@ -15,38 +15,38 @@ function page() {
 
 
             </div>
-       <div className='flex flex-col justify-around m-4 md:m-16 md:flex-row gap-y-6 md:gap-y-0 '>
-       <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-4 py-2  gap-2 rounded-sm'>
-                Delhi
-                <FontAwesomeIcon className='mx-2' icon={faMapLocationDot} />
+            <div className='flex flex-col justify-around m-4 md:m-16 md:flex-row gap-y-6 md:gap-y-0 '>
+                <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-4 py-2  gap-2 rounded-sm'>
+                    Delhi
+                    <FontAwesomeIcon className='mx-2' icon={faMapLocationDot} />
 
-            </Link>
-
-
-            <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-4 py-2  gap-2 rounded-sm'>
-                Maharashtra
-                <FontAwesomeIcon className='mx-2' icon={faMapLocationDot} />
-
-            </Link>
+                </Link>
 
 
+                <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-4 py-2  gap-2 rounded-sm'>
+                    Maharashtra
+                    <FontAwesomeIcon className='mx-2' icon={faMapLocationDot} />
 
-            <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-4 py-2  gap-2 rounded-sm'>
-                Goa
-                <FontAwesomeIcon className='mx-2' icon={faMapLocationDot} />
-
-            </Link>
-
-            <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-4 py-2  gap-2 rounded-sm'>
-            Rajasthan
-                <FontAwesomeIcon className='mx-2' icon={faMapLocationDot} />
-
-            </Link>
+                </Link>
 
 
-       </div>
 
-            
+                <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-4 py-2  gap-2 rounded-sm'>
+                    Goa
+                    <FontAwesomeIcon className='mx-2' icon={faMapLocationDot} />
+
+                </Link>
+
+                <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-4 py-2  gap-2 rounded-sm'>
+                    Rajasthan
+                    <FontAwesomeIcon className='mx-2' icon={faMapLocationDot} />
+
+                </Link>
+
+
+            </div>
+
+
             <Footer />
 
 

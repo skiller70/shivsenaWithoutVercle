d061c0cae4fd3f39f43419ca872558b798887ae0
lucky <singhlucky007007@gmail.com>
2023-05-28 07:34:34 +0530

    added alt property

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -22,8 +22,8 @@ function Loading() {
             <div className={`w-full h-[100%] fixed inset-0 bg-[#FF6634] ease-in-out z-50  ${initialLoading ? "translate-x-full  duration-1000" : " translate-x-0  duration-1000"}  transition  flex items-center `}>
 
 
-                <Image src="/initialLoading.png" width={200} height={200} className={`${initialLoading ? " duration-200  opacity-0" : " opacity-100"} `} />
-                <Image src="/finalArrow.png" width={200} height={400} className={`-ml-[15%] ${initialLoading ? "   duration-200  opacity-100" : " opacity-0"} `} />
+                <Image src="/initialLoading.png" width={200} height={200} className={`${initialLoading ? " duration-200  opacity-0" : " opacity-100"} `}  alt="initial "/>
+                <Image src="/finalArrow.png" width={200} height={400} className={`-ml-[15%] ${initialLoading ? "   duration-200  opacity-100" : " opacity-0"} `} alt="arrow" />
             </div>
 
 

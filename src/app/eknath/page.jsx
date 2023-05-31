6acdefe60c8c8a4dba76b5ca196edb24801cbf40
lucky <singhlucky007007@gmail.com>
2023-06-01 05:06:38 +0530

    eknath shinde remove dot1

--- src/app/eknath/page.jsx
+++ src/app/eknath/page.jsx
@@ -16,14 +16,14 @@ The 58-year-old was elected to the Maharashtra Assembly for four consecutive ter
 
 Following his victory in 2014, Shinde, the most popular leader was chosen to represent the Shiv Sena legislative party. He later served as the Leader of Opposition in the Maharashtra Assembly for a short time. In the Sena-BJP government, he was appointed PWD minister. On 30th June 2022, Eknath Shinde was sworn in as the 20th Chief Minister of Maharashtra.
 
-Shinde ji was introduced to politics by then Thane Shiv Sena President Anand Dighe in early 1980 and became successor to the latter's legacy after his death in 2001. He rose through the ranks after launching a labour union for the party. His position in the Shiv Sena became stronger after Bal Thackeray's nephew Raj left the party and launched Maharashtra Navnirman Sena." img={"/eknathStory.jpg"}/>
+Shinde ji was introduced to politics by then Thane Shiv Sena President Anand Dighe in early 1980 and became successor to the latters legacy after his death in 2001. He rose through the ranks after launching a labour union for the party. His position in the Shiv Sena became stronger after Bal Thackerays nephew Raj left the party and launched Maharashtra Navnirman Sena." img={"/eknathStory.jpg"}/>
 
 <div className=' mx-10 mb-10 md:mx-16 md:px-3 '>
 <h1 className=' text-2xl font-medium'>Humble Beginnings
 </h1>
 <br/>
 <p  className='  text-justify'>
-Eknath Shinde is a Maratha who was born in the Jawali taluka of Satara, Maharashtra in 9 February 1964. His family relocated to Thane, on Mumbai's outskirts, in order to make a living. Eknath attended Mangala High School & Junior College in Thane until the 11th grade.    
+Eknath Shinde is a Maratha who was born in the Jawali taluka of Satara, Maharashtra in 9 February 1964. His family relocated to Thane, on Mumbais outskirts, in order to make a living. Eknath attended Mangala High School & Junior College in Thane until the 11th grade.    
 
 After completing his education, Shinde Ji began working in the accounts department of the Municipal Corporation of Greater Mumbai. He later joined the Shiv Sena, a party founded by his uncle Bal Thackeray, and quickly rose through the ranks to become one of its senior leaders.</p>
 </div>

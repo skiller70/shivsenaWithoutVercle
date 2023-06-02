a2d5ed1edf44c42e2905846f771d17a92db88d6e
lucky <singhlucky007007@gmail.com>
2023-06-03 01:25:30 +0530

    change size

--- src/app/constitution/page.jsx
+++ src/app/constitution/page.jsx
@@ -38,41 +38,42 @@ function page() {
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
       >
+        <div className='text-2xl text-center my-32'>Constitution</div>
 
         <SwiperSlide><Image src="/constitution.jpg" height={500
         } width={800} quality={100} /></SwiperSlide>
         <SwiperSlide><Image src="/constitution1.jpg" height={500
-        } width={500} quality={100} alt='constitution1' /></SwiperSlide>
+        } width={800} quality={100} alt='constitution1' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution2.jpg" height={500
-        } width={500} quality={100} alt='constitution2' /></SwiperSlide>
+        } width={800} quality={100} alt='constitution2' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution3.jpg" height={500
-        } width={500} quality={100} alt='constitution3' /></SwiperSlide>
+        } width={800} quality={100} alt='constitution3' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution4.jpg" height={500
-        } width={500} alt='constitution4' /></SwiperSlide>
+        } width={800} alt='constitution4' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution5.jpg" height={500
-        } width={500} alt='constitution5' /></SwiperSlide>
+        } width={800} alt='constitution5' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution6.jpg" height={500
-        } width={500} alt='constitution6' /></SwiperSlide>
+        } width={800} alt='constitution6' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution7.jpg" height={500
-        } width={500} alt='constitution7' /></SwiperSlide>
+        } width={800} alt='constitution7' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution8.jpg" height={500
-        } width={500} alt='constitution8' /></SwiperSlide>
+        } width={800} alt='constitution8' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution9.jpg" height={500
-        } width={500} alt='constitution9' /></SwiperSlide>
+        } width={800} alt='constitution9' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution10.jpg" height={500
-        } width={500} alt='constitution10' /></SwiperSlide>
+        } width={800} alt='constitution10' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution11.jpg" height={500
-        } width={500} alt='constitution11' /></SwiperSlide>
+        } width={800} alt='constitution11' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution12.jpg" height={500
-        } width={500} alt='constitution12' /></SwiperSlide>
+        } width={800} alt='constitution12' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution13.jpg" height={500
-        } width={500} alt='constitution13' /></SwiperSlide>
+        } width={800} alt='constitution13' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution14.jpg" height={500
-        } width={500} alt='constitution14' /></SwiperSlide>
+        } width={800} alt='constitution14' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution15.jpg" height={500
-        } width={500} alt='constitution15' /></SwiperSlide>
+        } width={800} alt='constitution15' /></SwiperSlide>
         <SwiperSlide><Image src="/constitution16.jpg" height={500
-        } width={500} alt='constitution16' /></SwiperSlide>
+        } width={800} alt='constitution16' /></SwiperSlide>
 
 
 
@@ -82,16 +83,7 @@ function page() {
 
 
 
-      <div className='text-2xl text-center my-32'>Constitution
-
-
-
-
-
-
-
-
-      </div>
+      
 
 
       <Footer />

b6e570894399ef7c0ada0bb6e9d41580007b829f
lucky <singhlucky007007@gmail.com>
2023-06-06 03:03:41 +0530

    added link in media rescourcec

--- src/app/medialist/page.jsx
+++ src/app/medialist/page.jsx
@@ -10,24 +10,24 @@ function Page() {
             <div className='text-2xl font-medium my-8 text-center '>
                 Media Resource
                 <div className=' mx-16 my-16 grid grid-cols-1 md:grid-cols-3 gap-x-16 gap-y-8'>
-                    <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-2 py-2  gap-2 rounded-sm'>
+                    <Link href="/pressrealese" className='text-lg items-center bg-[#ffa500] text-white px-2 py-2  gap-2 rounded-sm'>
                         Press Releases
 
                     </Link>
-                    <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-2 py-2  gap-2 rounded-sm'>
+                    <Link href="/speeches" className='text-lg items-center bg-[#ffa500] text-white px-2 py-2  gap-2 rounded-sm'>
                         Speeches
 
                     </Link>
-                    <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-2 py-2  gap-2 rounded-sm'>
+                    <Link href="/intervewarticles" className='text-lg items-center bg-[#ffa500] text-white px-2 py-2  gap-2 rounded-sm'>
                         Interviews and Articles
 
                     </Link>
 
-                    <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-2 py-2  gap-2 rounded-sm'>
+                    <Link href="/photogallery" className='text-lg items-center bg-[#ffa500] text-white px-2 py-2  gap-2 rounded-sm'>
                         Photo Gallery
 
                     </Link>
-                    <Link href="/" className='text-lg items-center bg-[#ffa500] text-white px-2 py-2  gap-2 rounded-sm'>
+                    <Link href="/videogallery" className='text-lg items-center bg-[#ffa500] text-white px-2 py-2  gap-2 rounded-sm'>
                         Video Gallery
 
 

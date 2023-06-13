1032eb16ccc210716db5250753c3ec10ccd3f6de
lucky <singhlucky007007@gmail.com>
2023-06-13 06:44:54 +0530

    changed card img

--- src/app/page.js
+++ src/app/page.js
@@ -41,37 +41,37 @@ export default function Home() {
         </div> */}
 
 
-      <div className=' relative mt-4 md:mt-12   '>
-        <Link href="/ananddighe" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] '>View Story</Link>
-        <Image src="/anandDigheNew.jpg" width={1920} height={1080} alt='ananddighe' />
+      <div className=' relative mt-4 md:mt-12  w-[100%] h-[800px]  '>
+        <Link href="/ananddighe" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] z-10 '>View Story</Link>
+        <Image src="/anandDigheNew.jpg" fill={true} alt='ananddighe' />
       </div>
 
-      <div className=' relative mt-4 md:mt-12   '>
-        <Link href="/eknath" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] '>View Story</Link>
-        <Image src="/eknathNew.jpg" width={1920} height={1080} alt='eknath' />
+      <div className=' relative mt-4 md:mt-12   w-[100%] h-[850px] '>
+        <Link href="/eknath" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] z-10 '>View Story</Link>
+        <Image src="/eknathNew.jpg" fill={true} alt='eknath' />
       </div>
 
-      <div className=' relative mt-4 md:mt-12   '>
-        <Link href="/anandrao" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] '>View Story</Link>
-        <Image src="/anandRaoNew.jpg" width={1920} height={800} alt='anandrao' />
+      <div className=' relative mt-4 md:mt-12 w-[100%] h-[850px]  '>
+        <Link href="/anandrao" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] z-10'>View Story</Link>
+        <Image src="/anandRaoNew.jpg" fill={true} alt='anandrao' />
       </div>
 
-      <div className=' relative mt-4 md:mt-12   '>
-        <Link href="/rahulji" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] '>View Story</Link>
-        <Image src="/rahulNew.jpg" width={1920} height={800} alt='shrikant' />
-      </div>
-
-
-
-      <div className=' relative mt-4 md:mt-12'>
-        <Link href="/abhijeetji" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] '>View Story</Link>
-        <Image src="/abhijitNew.jpg" width={1920} height={800} alt='shrikant' />
+      <div className=' relative mt-4 md:mt-12 w-[100%] h-[850px]  '>
+        <Link href="/rahulji" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] z-10'>View Story</Link>
+        <Image src="/rahulNew.jpg" fill={true} alt='shrikant' />
       </div>
 
 
-      <div className=' relative mt-4 md:mt-12   '>
-        <Link href="/shrikant" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] '>View Story</Link>
-        <Image src="/shrikantNew.jpg" width={1920} height={800} alt='shrikant' />
+
+      <div className=' relative mt-4 md:mt-12 w-[100%] h-[850px] '>
+        <Link href="/abhijeetji" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] z-10 '>View Story</Link>
+        <Image src="/abhijitNew.jpg" fill={true} alt='shrikant' />
+      </div>
+
+
+      <div className=' relative mt-4 md:mt-12 w-[100%] h-[850px]  '>
+        <Link href="/shrikant" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] z-10'>View Story</Link>
+        <Image src="/shrikantNew.jpg" fill={true} alt='shrikant' />
       </div>
 
     
@@ -79,7 +79,7 @@ export default function Home() {
       <div className='grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 p-10 w-full gap-y-8  mt-16'>
         <CardImg img="img1.jpeg" alt="img1" />
         <CardImg img="img2.jpg" alt="img2" />
-        <CardImg img="img3.jpeg" alt="img3" />
+        <CardImg img="Img3.jpeg" alt="img3" />
       </div>
 
       {/* IMAGE CARD */}
@@ -87,7 +87,7 @@ export default function Home() {
       {/* CARD */}
       <div className='grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 p-10 w-full gap-y-24 mb-20 '>
         <Card img="cardImg1.jpeg" title="SHIV SENA" text="Shiv Sena launches Delhi unit, Eknath Shinde to visit soon" link="https://www.thehindu.com/news/cities/mumbai/shiv-sena-launches-delhi-unit-eknath-shinde-to-visit-soon/article66798329.ece#:~:text=With%20the%20launch%20of%20the,have%20a%20Shiv%20Sena%20unit&text=The%20Shiv%20Sena%2C%20led%20by,development%20in%20the%20national%20capital." alt="cardImg1" isAdmin={isAdminLogin} />
-        <Card img="cardImg2.jpeg" title="SHIV SENA" text="Eknath Shinde-led Shiv Sena launches Delhi unit, vows to apply ‘Maharashtra model’" link="https://www.news18.com/politics/shiv-sena-launches-delhi-unit-eknath-shinde-to-visit-soon-7683901.html" alt="cardImg2" isAdmin={isAdminLogin} />
+        <Card img="cardimg2New.jpg" title="SHIV SENA" text="Eknath Shinde-led Shiv Sena launches Delhi unit, vows to apply ‘Maharashtra model’" link="https://www.news18.com/politics/shiv-sena-launches-delhi-unit-eknath-shinde-to-visit-soon-7683901.html" alt="cardImg2" isAdmin={isAdminLogin} />
         <Card img="cardImg3.jpeg" title="SHIV SENA" text="With the launch of the Delhi unit, the national capital becomes the 20th State/Union Territory to have a Shiv Sena unit" link="https://www.livemint.com/politics/policy/eknath-shinde-led-shiv-sena-launches-delhi-unit-vows-to-apply-maharashtra-model-11682856855768.html" alt="cardImg3" isAdmin={isAdminLogin} />
 
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ import React from 'react'
 function Hero() {
   return (
     <div>
-       <div  className=' relative   '>
-        <Link href="/balasaheb" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%] '>View Story</Link>
-       <Image src="/balasahebNew.jpg" width={1920} height={1080} alt="hero"/>
+       <div  className=' relative w-[100%] h-[850px]    '>
+        <Link href="/balasaheb" className='absolute bg-[#ffa500] text-white px-4 py-2 rounded-md bottom-[7%] right-[10%]  z-10'>View Story</Link>
+       <Image src="/balasahebNew.jpg" fill={true} alt="hero"/>
        </div>
 
 
